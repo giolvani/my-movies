3345bd3517a1360ddb13a33dfdc8ebc2bcefee06
Giolvani de Matos <inavloig@gmail.com>
2025-06-16 22:06:28 -0300

    add layout to home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from '@/components/Home/Home';
+import Layout from '@/pages/Layout';
+
+const HomePage: React.FC = () => {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+};
+
+export default HomePage;
